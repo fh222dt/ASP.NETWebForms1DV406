c662e4de526a0f8ec8f70a0b3b4713f23d74b7e9
fh222dt <fh222dt@student.lnu.se>
2014-01-29 14:25:21 +0100

    secretnumber-klassen

--- steg 1/labb4/Labb4Steg1/Labb4Steg1/App_Code/SecretNumber.cs
+++ steg 1/labb4/Labb4Steg1/Labb4Steg1/App_Code/SecretNumber.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Web;
 
@@ -9,41 +10,129 @@ namespace Labb4Steg1.App_Code
     {
         //fält vad ska va privat??
         private int _number;
-        private int _previousGuesses;
-
+        private List<int> _previousGuesses;
+        //konstant
         public const int MaxNumberOfGuesses = 7;
 
+
         //Egenskaper
-        public bool CanMakeGuess { get; }
+        public bool CanMakeGuess 
+        {
+            get
+            {
+                if (Outcome == Outcome.NoMoreGuesses)
+                {
+                    return false;
+                }
+                else if (Outcome == Outcome.Correct)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
 
-        public int Count { get; }
+        public int Count 
+        { 
+            get { return _previousGuesses.Count; } 
+        }
 
-        public int Number { get; }  //finns ett litet frågetecken bakom klassdiagrammet på denna
+        public int? Number //är nullable
+        {
+            get
+            {
+                if (CanMakeGuess == true)
+                {
+                    return null;
+                }
+                else
+                {
+                    return _number;
+                }
+            }
+            //behövs set?
+        }  
 
-        public Outcome Outcome { get; private set; }        
+        public Outcome Outcome { get; private set; }
 
-        public IEnumerable PreviousGuesses { get; }
+        public ReadOnlyCollection<int> PreviousGuesses
+        {
+            get
+            {
+                ReadOnlyCollection<int> prevGuesses = new ReadOnlyCollection<int>(_previousGuesses);
+                return prevGuesses;
+            }
+        }
 
         //konstruktor
         public SecretNumber()
         {
-
+            Initialize();
+            _previousGuesses = new List<int>(7);
         }
 
         //Metoder
-        public static void Initialize()
+        public void Initialize()    //static?
         {
-            _number = new Random(1, 101);
-            _previousGuesses.Clear; 
+            Random random = new Random();
+            _number = random.Next(1, 101);
+            _previousGuesses.Clear();
+            Outcome = Outcome.Indefinite;
         }
 
-        public static int MakeGuess(int guess)
+        public Outcome MakeGuess(int guess)     //static??
         {
-            return; //returnera Outcome
+            if (guess > 0 && guess < 101)
+            {
+
+                if (Count == MaxNumberOfGuesses)
+                {
+                    Outcome = Outcome.NoMoreGuesses;
+                    return Outcome;
+                }
+                if (guess == _number)
+                {
+                    Outcome = Outcome.Correct;
+                    return Outcome;
+                }
+                if (_previousGuesses.Contains(guess))
+                {
+                    Outcome = Outcome.PreviousGuesses;
+                    return Outcome;
+                }
+                if (guess < _number)
+                {
+                    Outcome = Outcome.Low;
+                    return Outcome;
+                }
+                if (guess > _number)
+                {
+                    Outcome = Outcome.High;
+                    return Outcome;
+                }
+                else if (PreviousGuesses.Contains(guess))
+                {
+                    Outcome = Outcome.PreviousGuesses;
+                    return Outcome;
+                }
+                else
+                {
+                    Outcome = Outcome.Indefinite;
+                    return Outcome;
+                }
+
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("guess", "talet måste vara mellan 1-100");
+            }
 
         }
     }
 
     //uppräkningsbar typ Outcome
-
+    public enum Outcome { Indefinite, Low, High, Correct, NoMoreGuesses, PreviousGuesses };
 }
